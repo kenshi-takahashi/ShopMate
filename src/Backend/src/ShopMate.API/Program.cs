76e56418e0cb89a4ad2e619cd1634bb40351b739
kenshi-takahashi <suslikgory12345555@gmail.com>
2025-08-06 13:21:33 +0300

    feat(API): add Swagger

--- src/Backend/src/ShopMate.API/Program.cs
+++ src/Backend/src/ShopMate.API/Program.cs
@@ -2,6 +2,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 builder.Configuration.AddEnvironmentVariables();
 
 var app = builder.Build();
@@ -9,8 +11,11 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
 
 app.Run();
+
