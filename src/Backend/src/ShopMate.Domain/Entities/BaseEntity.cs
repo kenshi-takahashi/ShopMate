d0355d9040a4cb012e007c86c7bdd52a97fe53c9
kenshi-takahashi <suslikgory12345555@gmail.com>
2025-08-08 09:32:43 +0300

    refactor(Domain): change UpdatedAt property type to DateTime in the BaseEntity.cs

--- src/Backend/src/ShopMate.Domain/Entities/BaseEntity.cs
+++ src/Backend/src/ShopMate.Domain/Entities/BaseEntity.cs
@@ -4,6 +4,6 @@ namespace ShopMate.Domain.Entities
     {
         public Guid Id { get; set; }
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-        public DateTime? UpdatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
     }
 }
