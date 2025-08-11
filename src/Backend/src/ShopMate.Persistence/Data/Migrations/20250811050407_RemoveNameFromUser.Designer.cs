4d9e75c8fb249e0ada8a65cf6ddbcb23bff69526
kenshi-takahashi <suslikgory12345555@gmail.com>
2025-08-11 08:24:31 +0300

    feat(migrations): add a migration to remove the FirstName and LastName properties from the User entity

--- src/Backend/src/ShopMate.Persistence/Data/Migrations/20250811050407_RemoveNameFromUser.Designer.cs
+++ src/Backend/src/ShopMate.Persistence/Data/Migrations/20250811050407_RemoveNameFromUser.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using ShopMate.Persistence.Data;
@@ -11,9 +12,11 @@ using ShopMate.Persistence.Data;
 namespace ShopMate.Persistence.Data.Migrations
 {
     [DbContext(typeof(ShopMateDbContext))]
-    partial class ShopMateDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250811050407_RemoveNameFromUser")]
+    partial class RemoveNameFromUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
